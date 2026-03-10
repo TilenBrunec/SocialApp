31ae76d4af2f7d0bde65a113c7b3f80cec15a93d
TilenBrunec <tilen.brunec@student.um.si>
2026-03-10 20:02:21 +0100

    sklop 1 koncanstil dodal

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,15 +15,39 @@ namespace SocialApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Objava
+    {
+        public string Avtor { get; set; } = "";
+        public string Vsebina { get; set; } = "";
+        public string Datum { get; set; } = "";
+        public int Likes { get; set; } = 0;
+
+    }
+
+
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Objava> objave = new();
         public MainWindow()
         {
             InitializeComponent();
+            ListViewObjave.ItemsSource = objave;
+
+            objave.Add(new Objava { Avtor = "Tiki Eld", Vsebina = "Prva objava!", Datum = "11.3.2026", Likes = 3 });
+            objave.Add(new Objava { Avtor = "Tiki Eld", Vsebina = "Druga objava!", Datum = "11.3.2026", Likes = 7 });
         }
 
-        
+
         private void MenuIzhodClick(object sender, RoutedEventArgs e)
               => Application.Current.Shutdown();
+
+        private void ListViewObjave_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (ListViewObjave.SelectedItem is Objava objava
+                )
+            {
+                MessageBox.Show(objava.Vsebina, "to je vsebina");
+            }
+        }
     }
 }
