94d6058bfc5f9bbb8f7e7db7c4398fda7ce310a3
TilenBrunec <tilen.brunec@student.um.si>
2026-03-25 19:59:07 +0100

    naloga 3 dodana

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,12 +16,57 @@ namespace SocialApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public class Objava
+    public class Objava : INotifyPropertyChanged
+
     {
-        public string Avtor { get; set; } = "";
-        public string Vsebina { get; set; } = "";
-        public string Datum { get; set; } = "";
-        public int Likes { get; set; } = 0;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string avtor = "";
+        private string vsebina = "";
+        private string datum = "";
+        private int likes = 0;
+        private string slika = "";
+        private string kategorija = "";
+
+        public string Avtor {
+
+            get { return avtor; }
+            set { avtor = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Avtor")); }
+        }
+
+        public string Vsebina {
+
+            get { return vsebina; }
+            set { vsebina = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Vsebina")); }
+        }
+
+        public string Datum {
+
+            get { return datum; }
+            set { datum = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Datum")); }
+        }
+
+        public int Likes {
+
+            get { return likes; }
+            set { likes = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Likes"));  PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BrezVseckov"));
+            }
+        }
+
+        public string Slika {
+
+            get { return slika; }
+            set { slika = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Slika")); }
+        }
+
+        public string Kategorija {
+
+            get { return kategorija; }
+            set { kategorija = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Kategorija")); }
+        }
+
+       
+        public bool BrezVseckov => likes == 0;
+
 
     }
 
@@ -33,21 +79,26 @@ namespace SocialApp
             InitializeComponent();
             ListViewObjave.ItemsSource = objave;
 
-            objave.Add(new Objava { Avtor = "Tiki Eld", Vsebina = "Prva objava!", Datum = "11.3.2026", Likes = 3 });
-            objave.Add(new Objava { Avtor = "Tiki Eld", Vsebina = "Druga objava!", Datum = "11.3.2026", Likes = 7 });
+            objave.Add(new Objava { Avtor = "Tiki Eld", Vsebina = "Pozdravljen svet!", Datum = "10.3.2026", Likes = 5, Slika = "/Slike/profile.jpg", Kategorija = "Splošno" });
+            objave.Add(new Objava { Avtor = "Tiki Eld", Vsebina = "Danes brez všečkov.", Datum = "11.3.2026", Likes = 0, Slika = "/Slike/profile.jpg", Kategorija = "Osebno" });
+            objave.Add(new Objava { Avtor = "Tiki Eld", Vsebina = "Programiranje je zabavno!", Datum = "12.3.2026", Likes = 12, Slika = "/Slike/profile.jpg", Kategorija = "Tehnologija" });
         }
-
 
         private void MenuIzhodClick(object sender, RoutedEventArgs e)
               => Application.Current.Shutdown();
 
-        private void ListViewObjave_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (ListViewObjave.SelectedItem is Objava objava
-                )
-            {
+        private void ListViewObjave_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            if (ListViewObjave.SelectedItem is Objava objava) {
                 MessageBox.Show(objava.Vsebina, "to je vsebina");
             }
         }
+        private void BtnVseckaj_Click(object sender, RoutedEventArgs e) {
+            if (ListViewObjave.SelectedItem is Objava o) {
+                o.Likes++;
+            }else {
+                MessageBox.Show("Izberi objavo", "Opozotrilo");
+            }
+        }
+
     }
 }
